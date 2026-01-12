b52a5def70c0cdc15665754a97efed87bb76cc2b
slwnt31 <20241005@sungshin.ac.kr>
2026-01-12 23:52:02 +0900

    Refactor: LoginSerializer 주석 삭제 (#7)

--- users/serializers.py
+++ users/serializers.py
@@ -19,12 +19,3 @@ class SignupSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
-
-    # def validate(self, data):
-    #     # USERNAME_FIELD(email) 기반으로 인증
-    #     user = authenticate(**{User.USERNAME_FIELD: data["email"], "password": data["password"]})        
-    #     if not user:
-    #         raise serializers.ValidationError("이메일 또는 비밀번호가 일치하지 않습니다.")
-        
-    #     data['user'] = user
-    #     return data
