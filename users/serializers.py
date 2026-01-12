22dfb1eebe64bd6bbbda7ada969f49318ce8b8d0
slwnt31 <20241005@sungshin.ac.kr>
2026-01-12 22:02:26 +0900

    Feat: 로그인 구현 / views 에러 처리 (#7)

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from .services import create_user
 
 User = get_user_model()
@@ -15,3 +15,16 @@ class SignupSerializer(serializers.ModelSerializer):
     # 생성된 유저 검증
     def create(self, validated_data):
         return create_user(**validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    # def validate(self, data):
+    #     # USERNAME_FIELD(email) 기반으로 인증
+    #     user = authenticate(**{User.USERNAME_FIELD: data["email"], "password": data["password"]})        
+    #     if not user:
+    #         raise serializers.ValidationError("이메일 또는 비밀번호가 일치하지 않습니다.")
+        
+    #     data['user'] = user
+    #     return data
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model, authenticate
 
-from .serializers import SignupSerializer
+from .serializers import SignupSerializer, LoginSerializer
 
 User = get_user_model()
 
@@ -10,3 +14,43 @@ class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignupSerializer
     permission_classes = [AllowAny]
+    
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        # 입력 데이터 -> serializer로 검증
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+        
+        if not User.objects.filter(email=email).exists():
+            return Response(
+                { "message" : "해당 이메일의 계정이 존재하지 않습니다."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        user = authenticate(**{User.USERNAME_FIELD: email, "password": password})
+        if not user:
+            return Response(
+                { "message" : "이메일 또는 비밀번호가 일치하지 않습니다."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # JWT 발급
+        refresh = RefreshToken.for_user(user)
+        
+        return Response(
+            {
+                "message" : "로그인 되었습니다.",
+                "user": {
+                    "username" : user.username,
+                    "email" : user.email,  
+                },
+                "access" : str(refresh.access_token),
+                "refresh(로그인 연장 시 필요)" : str(refresh),
+            },
+            status=status.HTTP_200_OK,
+        )
