8ed31a04229cf8f7973a3d7a8c8217c6c8fb4b0c
slwnt31 <20241005@sungshin.ac.kr>
2026-01-13 03:54:28 +0900

    Feat: 회원 탈퇴 구현 (#15)

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import SignupView, LoginView, LogoutView, UserUpdateView
+from .views import SignupView, LoginView, LogoutView, UserUpdateView, UserDeleteView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('me/', UserUpdateView.as_view(), name='user-me'),
-    path('profile/', UserUpdateView.as_view(), name="user-profile"),
+    
+    # 수정
+    path('profile-update/', UserUpdateView.as_view(), name='profile-update'),
+    
+    # 조회
+    path('profile/', UserUpdateView.as_view(), name="profile"),
+    
+    # 삭제
+    path('profile-delete/', UserDeleteView.as_view(), name="profile-delete"),
 ]
