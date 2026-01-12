6bb6da411959d1485a151a5ab5df2aae052e0895
slwnt31 <20241005@sungshin.ac.kr>
2026-01-13 02:26:43 +0900

    Refactor: 비밀번호 확인용 변수 추가, 입력 글자 제한, 검증 함수 추가 (#8)

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,30 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from .services import create_user
 
 User = get_user_model()
 
 
 class SignupSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8)
+    password2 = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password")
+        fields = ("username", "email", "password", "password2")
 
+    def validate(self, data):
+        pw1 = data.get("password")
+        pw2 = data.get("password2")
+        if pw1 != pw2:
+            raise serializers.ValidationError("입력한 비밀번호가 일치하지 않습니다.")
+        validate_password(pw1)
+        return data
+        
     # 생성된 유저 검증
     def create(self, validated_data):
+        validated_data.pop("password2")
         return create_user(**validated_data)
 
 class LoginSerializer(serializers.Serializer):
