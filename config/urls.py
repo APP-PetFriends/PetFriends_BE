97a12f0f7173c5597897993ffcfa85662c716236
slwnt31 <20241005@sungshin.ac.kr>
2026-01-13 16:47:08 +0900

    Feat: image url 설정 및 pet url 수정 (#17)

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/drug/', include('drug.urls')),
     # path('api/health/', include('health.urls')),
-    # path('api/pet/', include('pet.urls')),
+    path('api/', include('pet.urls')),
     path('api/users/', include('users.urls')),
     # path('api/walk/', include('walk.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
